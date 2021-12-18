e94c5ee750110a9cd89428e98ac68ec1023767de
Nicolas Chabloz <43348620+Chabloz@users.noreply.github.com>
2021-12-18 23:32:31 +0100

    Create d_18.js

---
+++ d_18.js
@@ -0,0 +1,164 @@
+const input = '[[[[7,1],[0,0]],[6,[8,2]]],[8,[3,8]]] [[[3,6],[9,4]],[[[5,9],5],[8,0]]] [[[2,2],2],[1,[[1,6],7]]] [[[[0,9],7],[[3,2],8]],[6,[7,9]]] [[[[4,1],6],[[7,6],[2,2]]],[[[1,1],9],4]] [[[8,[3,7]],3],[[4,4],[[9,1],[3,5]]]] [[4,[8,2]],[1,[0,5]]] [8,[8,7]] [[[[2,2],7],[3,[4,5]]],[[4,6],[[2,5],4]]] [[[5,5],[[5,1],3]],[[2,[8,2]],[[6,9],[1,5]]]] [0,7] [[[[5,1],3],[8,[5,3]]],7] [[5,[2,[0,6]]],[[[5,5],2],[9,[8,0]]]] [[[[3,4],2],0],4] [[[[5,3],[2,7]],6],[[4,0],[9,[7,2]]]] [[[3,[2,5]],[3,3]],7] [[[[5,1],1],[4,8]],[[5,[8,3]],2]] [[4,[[8,1],[8,5]]],[[[4,1],0],6]] [[[5,5],[5,9]],[0,[[6,8],[0,1]]]] [4,[[[7,9],4],0]] [[[[0,1],7],[[3,6],5]],[8,[5,[6,1]]]] [[[7,7],[8,0]],[6,[8,[7,9]]]] [[[9,2],1],6] [[[4,4],[2,[5,0]]],[[[2,6],6],[5,[4,3]]]] [[2,[[4,7],5]],1] [[8,7],[[[2,0],7],[1,[0,3]]]] [[9,[[9,3],[9,5]]],[[8,7],[[4,1],[6,5]]]] [[3,4],[[9,4],5]] [[5,[[8,3],5]],1] [[0,[[9,0],[3,2]]],[2,[7,[5,1]]]] [[9,[[9,5],[8,6]]],[[4,4],[[3,8],[1,6]]]] [[[1,[5,2]],9],[[4,6],[3,[8,0]]]] [[1,7],[[1,7],9]] [[[[3,4],3],[[7,5],[9,1]]],[[[5,0],[3,0]],[[7,9],6]]] [[[7,2],[[1,0],[5,6]]],[[[3,7],[8,9]],6]] [[[[1,1],1],[[8,6],[9,8]]],[[[1,8],4],[8,9]]] [[[8,9],0],3] [[[1,7],[1,[3,9]]],[6,[0,[8,5]]]] [[0,5],[6,5]] [[[[6,8],[4,5]],[[7,4],6]],[[3,6],5]] [[8,[[0,9],8]],[9,[7,[7,9]]]] [0,[[[7,1],2],[[0,4],4]]] [[0,[[9,1],5]],[1,4]] [3,4] [[[9,3],[1,3]],[[[4,8],3],[[1,3],[9,0]]]] [[[[5,1],7],[[9,2],8]],[[[6,8],[5,4]],[0,1]]] [8,[[1,[3,0]],[[7,9],4]]] [[[6,4],[[2,9],[9,0]]],[7,[[0,0],3]]] [[3,[[9,6],6]],2] [[5,[[3,1],[7,5]]],[[[6,7],9],[[4,6],[5,2]]]] [[[4,[6,5]],8],[[6,[8,0]],[[9,3],3]]] [[[[4,9],[2,8]],9],[[[5,0],0],[[3,4],[2,8]]]] [[3,[7,1]],[9,[[1,8],7]]] [[9,1],[0,[[0,7],[7,1]]]] [[7,[0,[7,6]]],[[[5,3],1],[6,[4,5]]]] [8,[[[2,1],[6,9]],[[3,3],[4,6]]]] [0,[7,[3,0]]] [[[[1,6],3],[5,[8,0]]],[[[6,6],7],1]] [[[7,[8,3]],3],[[[2,8],5],[0,[9,5]]]] [[[[5,1],4],[[1,2],1]],7] [[[3,[7,5]],7],3] [[9,[6,[1,1]]],[[[4,1],[2,2]],[[9,5],[7,7]]]] [2,7] [[[9,[8,6]],[[9,0],[6,5]]],[[[6,7],5],[[7,7],[2,3]]]] [[[0,[6,4]],2],[4,[7,[7,5]]]] [[[[6,1],[9,1]],[[6,1],9]],[[2,6],0]] [[0,[[1,8],[3,5]]],[4,[[8,2],[4,2]]]] [[[[9,3],[4,2]],2],[[[2,1],[7,1]],[4,8]]] [[[3,[0,2]],3],8] [[[4,[4,9]],9],[[[4,4],5],9]] [[[[8,2],7],9],[[[1,0],[3,8]],[[7,7],0]]] [[[3,2],[9,7]],[[9,[8,2]],[[5,5],3]]] [[[7,[3,1]],[[8,3],1]],[[[8,6],[7,0]],4]] [[9,[[9,1],5]],[[4,[1,1]],2]] [[[[7,4],[0,3]],7],[8,[6,[3,3]]]] [5,5] [[6,7],[1,[7,[8,1]]]] [[1,[0,4]],7] [[[4,0],[[0,1],[2,2]]],[9,[[9,9],[3,0]]]] [[[6,0],[[8,6],3]],[[5,1],[[8,1],[2,7]]]] [[[[8,3],7],5],[9,[[5,1],8]]] [[[[4,0],[5,2]],[[0,0],7]],2] [[[[0,1],6],2],[[8,2],6]] [[[[2,4],1],[[6,7],9]],[[[1,6],9],3]] [[5,5],[[8,[7,7]],[5,8]]] [[6,[[9,2],[9,7]]],[[[8,5],[4,4]],7]] [[[9,[7,7]],[6,0]],[7,[[8,7],[1,2]]]] [[7,[6,2]],[[9,[5,2]],[1,4]]] [[[7,[5,9]],[[3,9],[4,5]]],[0,6]] [[9,[8,[2,2]]],[[9,7],[1,1]]] [[[[2,3],4],[[4,8],9]],[[9,[8,6]],[[0,9],0]]] [[0,[[9,3],0]],[8,8]] [[[[2,9],6],[[2,8],9]],[[[0,5],6],[[6,1],7]]] [[9,[[8,3],[5,8]]],[[7,[3,0]],3]] [[[4,[4,2]],0],1] [[[[9,6],[5,8]],[6,2]],[[[8,0],[7,0]],[[5,6],4]]] [[[8,0],[[4,3],[7,4]]],[[3,[7,9]],[[7,3],6]]] [[3,[5,[0,3]]],[5,4]] [[[[1,2],[6,3]],1],[[7,[5,2]],[[8,8],7]]] [[4,[[8,0],[7,1]]],[[8,[8,0]],[[1,5],3]]]';
+
+const snailfishStrNumbers = input.split(' ')
+
+// str utility function
+function replaceAfterIndex(str, search, replace, index) {
+  return str.slice(0, index) + str.slice(index).replace(search, replace);
+}
+
+function addSnailfishNumbersStr(strNb1, strNb2) {
+  return `[${strNb1},${strNb2}]`;
+}
+
+function getValAfterIndex(strNb, index) {
+  const findIn = strNb.slice(index);
+  let mustTrackNext = false;
+  let val = '';
+  for (const charNb of findIn) {
+    if (charNb == ']') continue;
+    if (charNb == ',' && mustTrackNext) return val;
+    if (charNb == ',') {
+      mustTrackNext = true;
+      continue;
+    }
+    if (charNb == '[' && mustTrackNext) continue;
+    if (charNb == '[') return val;
+    val += charNb;
+  }
+  return val;
+}
+
+function getValBeforeIndex(strNb, index) {
+  let buffer = '';
+  let lastI = 0;
+  let stopNextSymbol = false;
+  for (let i = index; i>=0; i--) {
+    const charNb = strNb[i];
+    if (charNb == ']' && !stopNextSymbol) continue;
+    if (charNb == ',' && !stopNextSymbol) continue;
+    if (charNb == '[' && !stopNextSymbol) continue;
+    if (charNb == ']') break;
+    if (charNb == ',') break;
+    if (charNb == '[') break;
+    buffer = charNb + buffer;
+    lastI = i;
+    stopNextSymbol = true;
+  }
+  return [buffer, lastI];
+}
+
+function splitIntoPair(strNb, index, val) {
+  // console.log('we split' , val, ' here: ', index);
+  const val1 = Math.floor(parseInt(val) / 2);
+  const val2 = Math.ceil(parseInt(val) / 2);
+  return replaceAfterIndex(strNb, val, `[${val1},${val2}]`, index);
+}
+
+function reduceSnailfishNumberStr(strNb, explodeMode = true) {
+  let deep = 0;
+  let penultimateVal = '';
+  let val = '';
+  let mustExplode = false;
+  let lastOpeningBraceIndex = 0;
+  let lastCommaIndex = 0;
+  for (let i = 0; i < strNb.length; i++) {
+    const charNb = strNb[i];
+    switch (charNb) {
+      case '[':
+        lastOpeningBraceIndex = i;
+        deep++;
+        if (deep>=5) mustExplode = true;
+        continue
+      case ']':
+        if (mustExplode && explodeMode) {
+          // has a regular value to its right ?
+          const nextVal = getValAfterIndex(strNb, i);
+          if (nextVal) {
+            const newValue = parseInt(val) + parseInt(nextVal);
+            strNb = replaceAfterIndex(strNb, nextVal, newValue, i);
+          }
+          // has a regular value to its left ?
+          const [prevVal, iPrev] = getValBeforeIndex(strNb, lastOpeningBraceIndex);
+          if (prevVal) {
+            const newValue = parseInt(prevVal) + parseInt(penultimateVal);
+            strNb = replaceAfterIndex(strNb, prevVal, newValue, iPrev-1);
+          }
+          // replace the pair with 0
+          strNb = replaceAfterIndex(strNb, `[${penultimateVal},${val}]`, '0', lastOpeningBraceIndex);
+          return strNb;
+        }
+        // if the val is > 10, we must split
+        if (parseInt(val) >= 10  && !explodeMode) {
+          return splitIntoPair(strNb, lastCommaIndex, val);
+        }
+        penultimateVal = val;
+        val = '';
+        deep--;
+        continue;
+      case ',':
+        // if the val is > 10, we must split
+        if (parseInt(val) >= 10 && !explodeMode) {
+          return splitIntoPair(strNb, lastOpeningBraceIndex, val);
+        }
+        lastCommaIndex = i;
+        penultimateVal = val;
+        val = '';
+        continue;
+      default:
+        val += charNb;
+    }
+  }
+  return strNb;
+}
+
+function reduceAll(snailfishNumberStr) {
+  let lastSnailfishNumberStr = '';
+  while (true) {
+    while (lastSnailfishNumberStr != snailfishNumberStr) {
+      lastSnailfishNumberStr = reduceSnailfishNumberStr(snailfishNumberStr, true);
+      snailfishNumberStr = reduceSnailfishNumberStr(lastSnailfishNumberStr, true);
+    }
+    lastSnailfishNumberStr = reduceSnailfishNumberStr(snailfishNumberStr, false);
+    if (lastSnailfishNumberStr == snailfishNumberStr) break;
+    snailfishNumberStr = lastSnailfishNumberStr;
+    lastSnailfishNumberStr = '';
+  }
+  return snailfishNumberStr;
+}
+
+function calculateMagnitude(snailfishNumberStr) {
+  let array = JSON.parse(snailfishNumberStr);
+  return getMagnitude(array[0], 3) + getMagnitude(array.pop(), 2);
+}
+
+function getMagnitude(array, mulBy) {
+  if (!Array.isArray(array)) {
+    return mulBy * array;
+  }
+  return mulBy * (getMagnitude(array[0], 3) + getMagnitude(array.pop(), 2));
+}
+
+let snailfishNumberStr = snailfishStrNumbers[0];
+for (let i=1; i< snailfishStrNumbers.length; i++) {
+  snailfishNumberStr = addSnailfishNumbersStr(snailfishNumberStr, snailfishStrNumbers[i]);
+  snailfishNumberStr = reduceAll(snailfishNumberStr);
+}
+const magnitude = calculateMagnitude(snailfishNumberStr);
+console.log(magnitude);
+
+// part 2
+let maxMagnitude = -Infinity;
+for (let i=0; i< snailfishStrNumbers.length; i++) {
+  let snailfishNumberStr1 = snailfishStrNumbers[i];
+  for (let j=0; j< snailfishStrNumbers.length; j++) {
+    let snailfishNumberStr2 = snailfishStrNumbers[j];
+    let sum = addSnailfishNumbersStr(snailfishNumberStr1, snailfishNumberStr2);
+    let reduce = reduceAll(sum);
+    let magnitude = calculateMagnitude(reduce);
+    if (magnitude>maxMagnitude) {
+      maxMagnitude = magnitude;
+    }
+  }
+}
+console.log(maxMagnitude);
